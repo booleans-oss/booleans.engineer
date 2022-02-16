e60a4b9f659332f2c5e077134d6cfa8bfebeb328
booleans-oss <ztafade@gmail.com>
2022-02-16 16:50:24 +0400

    feat(client): add code editor

---
+++ components/CodeGroup.vue
@@ -0,0 +1,102 @@
+<template>
+   <div class="bloc">
+        <div class="header">
+             <div
+                v-for="({ label }, i) in tabs"
+                ref="tabs"
+                :key="label"
+                :class="[activeTabIndex === i && 'active']"
+                @click="updateTabs(i)" 
+            >
+               {{ label }}
+            </div>
+            <span ref="highlight-underline" class="highlight-underline" />
+        </div>
+        <slot />
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tabs: [],
+      activeTabIndex: 0,
+    }
+  },
+  watch: {
+    activeTabIndex(newValue, oldValue) {
+      this.switchTab(newValue)
+    },
+  },
+  mounted() {
+    this.tabs = this.$slots.default()
+    .filter(slot => Boolean(slot.el))
+      .map((slot) => {
+        return {
+            label: slot.props.label,
+            elm: slot.el,
+        }
+      })
+    this.$nextTick(this.updateHighlighteUnderlinePosition)
+  },
+  methods: {
+    switchTab(i) {
+       this.tabs.map((tab) => tab.elm.classList.remove('active'))
+      this.tabs[i].elm.classList.add('active')
+    },
+    updateTabs(i) {
+      this.activeTabIndex = i
+      this.updateHighlighteUnderlinePosition()
+    },
+    updateHighlighteUnderlinePosition() {
+      const activeTab = this.$refs.tabs[this.activeTabIndex]
+      if (!activeTab) {
+        return
+      }
+      const highlightUnderline = this.$refs['highlight-underline']
+      highlightUnderline.style.left = `${activeTab.offsetLeft}px`
+      highlightUnderline.style.width = `${activeTab.clientWidth}px`
+    },
+  },
+}
+</script>
+
+<style scoped lang="scss">
+.bloc {
+    width: 50%;
+    margin-top: 2rem;
+    background-color: rgba(45, 55, 72, 1.00);
+    border-radius: 5px;
+
+    .header {
+        display: inline-flex;
+        position: relative;
+        color: rgb(203, 213, 224);
+        font-size: 14px;
+        font-weight: 500;
+        width: 100%;
+        border-bottom: rgba(74, 85, 104, 1.00) 2px solid;
+        margin-bottom: -2px;
+        transition: left 150ms, width 150ms;
+
+        .active {
+            z-index: 99;
+        }
+        & > div {
+            cursor: pointer;
+            margin: 0 1rem;
+            margin-top: 0.5rem;
+            padding-bottom: 1px;
+        }
+        
+        .highlight-underline {
+        position: absolute;
+        background-color: green;
+        bottom: -2px;
+        height: 2px;
+        transition: left 150ms, width 150ms;
+        }
+    }
+}
+</style>
---
+++ pages/test.vue
@@ -0,0 +1,51 @@
+<template>
+<div class="container">
+    <CodeGroup>
+        <div class="code-bloc active" label="Yarn" active>
+           {{ "yarn \n yarn dev \n yarn build"}} 
+        </div>
+        <div class="code-bloc" label="NPM">
+           {{ "npm install \n npm run dev \n npm run build" }} 
+        </div>
+    </CodeGroup>
+    </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: 'page',
+})
+</script>
+
+<script lang="ts">
+import hljs from 'highlight.js';
+
+export default {
+    mounted() {
+        document.querySelectorAll('pre code')
+            .forEach((block) => {
+                block.innerHTML = hljs.highlight('bash', block.innerHTML).value;
+        })
+    }
+}</script>
+
+<style lang="scss">
+@import '~/assets/styles/highlights.scss';
+.container {
+    width: 100%;
+    margin-top: 2rem;
+    background-color: rgba(26, 32, 44, 1.00);
+
+    .code-bloc {
+        white-space: pre-line; 
+        font-family: DM Mono;
+        font-weight: 300;
+        padding: 1rem 2rem;
+        display: none;
+
+        &.active {
+            display: block;
+        }
+    }
+}
+</style>
