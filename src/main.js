5415569e11908896019d210088ae9e3ef509309e
booleans-oss <ztafade@gmail.com>
2022-03-02 20:44:30 +0400

    fix: fix imports order

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import { createRouter, createWebHistory } from 'vue-router'
 import './styles/globals.scss'
-
+import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import { createI18n } from 'vue-i18n'
-
-import IndexView from './pages/index.vue'
+import App from './App.vue'
 import ENLocales from './locales/en'
+import IndexView from './pages/index.vue'
 import FRLocales from './locales/fr'
 
 const routes = [{ path: '/', component: IndexView }]
