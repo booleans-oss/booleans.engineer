9cfac4ece119e89ec3658d399f1d7a34998d00f6
booleans-oss <ztafade@gmail.com>
2022-03-05 02:58:31 +0400

    chore(issues): fix code issues

--- src/scripts/date.js
+++ src/scripts/date.js
@@ -1,5 +1,10 @@
 import { format } from 'date-fns'
 
+/**
+ * Format given date to a string
+ * @param date The date string
+ * @returns formatted date
+ */
 export function formatDate(date) {
   return format(new Date(date), 'LLL d, yyyy')
 }
