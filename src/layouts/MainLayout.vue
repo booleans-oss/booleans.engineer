7ed49598faf7e8227a795f810f186f2f44f9b752
Clement Lebon <40752807+booleans-oss@users.noreply.github.com>
2022-04-12 15:46:51 +0400

    remove PanelBear

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,16 +1,5 @@
 <template>
   <div>
-  <head>
-    <script async src="https://cdn.panelbear.com/analytics.js?site=LiIA6fRzZgA"></script>
-    <script>
-        window.panelbear = window.panelbear || function() { (window.panelbear.q = window.panelbear.q || []).push(arguments); };
-        panelbear('config', {
-            site: 'LiIA6fRzZgA',
-            spaMode: 'history',
-            debug: false,
-        });
-    </script>
-  </head>
     <NavbarHeader />
     <div class="frame"></div>
     <div class="main">
